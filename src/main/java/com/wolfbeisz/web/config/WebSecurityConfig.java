ac0a5ee206b26edfb594e540b30a57c42d491fbb
unknown <philippwolfbeisz@gmail.com>
2014-10-13 14:26:15 +0200

    fixed bug: spring security's mechanism to prevent csrf broke the upload forms (csrf token is null); solution: disabled csrf

--- src/main/java/com/wolfbeisz/web/config/WebSecurityConfig.java
+++ src/main/java/com/wolfbeisz/web/config/WebSecurityConfig.java
@@ -1,29 +1,22 @@
 package com.wolfbeisz.web.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import com.wolfbeisz.model.User;
-import com.wolfbeisz.repository.UserRepository;
 
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	//disable csrf as it causes a problem with file uploads
+    	http.csrf().disable();
+    	
     	http
     		.authorizeRequests()
     			.antMatchers("/webjars/**").permitAll()
@@ -35,17 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.logout()
 				.permitAll();
-//        http
-//            .authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .anyRequest().authenticated();
-//        http
-//            .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//            .logout()
-//                .permitAll();
     }
 
     @Configuration
